682f7c4d6962bc554797b9a9ba2f464a33523f82
atikxcode <liadbd20@gmail.com>
2024-06-22 12:22:39 +0600

    footer created

--- src/Pages/Footer/Footer.jsx
+++ src/Pages/Footer/Footer.jsx
@@ -1,31 +1,112 @@
-import React from 'react';
+import { FaArrowRight, FaDribbble, FaFacebookF, FaInstagram, FaTwitter } from 'react-icons/fa';
+import image from '../../assets/logo_Asset-1-1.png'
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
   return (
-    <footer className="footer p-10 bg-base-300 text-base-content">
-  <nav>
-    <h6 className="footer-title">Services</h6> 
-    <a className="link link-hover">Branding</a>
-    <a className="link link-hover">Design</a>
-    <a className="link link-hover">Marketing</a>
-    <a className="link link-hover">Advertisement</a>
-  </nav> 
-  <nav>
-    <h6 className="footer-title">Company</h6> 
-    <a className="link link-hover">About us</a>
-    <a className="link link-hover">Contact</a>
-    <a className="link link-hover">Jobs</a>
-    <a className="link link-hover">Press kit</a>
-  </nav> 
-  <nav>
-    <h6 className="footer-title">Social</h6> 
-    <div className="grid grid-flow-col gap-4">
-      <a><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current"><path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"></path></svg></a>
-      <a><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current"><path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"></path></svg></a>
-      <a><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current"><path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path></svg></a>
+    <footer className="bg-[#0f0f0f] text-white pt-[200px]  mt-20">
+     
+      <div className='mx-auto container'>
+
+      <div className='flex justify-between'>
+
+        {/* first part */}
+
+        <div className='flex flex-col gap-8 w-1/2'>
+
+
+        <div className='flex items-center gap-4'>
+        <img className='w-[70px]' src={image} alt="" />
+        <h2 className='text-4xl font-normal'>Octalink Global Sourcing</h2>
+        </div>
+
+        <p className='text-gray-400 font-sans w-[350px] text-lg font-semibold mb-2 hover:text-white duration-500'>Join to our newsletter for the latest trends and updates in fashion and textiles!</p>
+         
+         
+
+         <div className='flex items-center'>
+         <input className="bg-inherit placeholder:text-white placeholder:font-normal placeholder:tracking-[2px] border-b-[1px] w-[300px] border-gray-600 focus:border-white hover:border-white  px-4 py-2 outline-none duration-500" placeholder="Your email" type="email" />
+
+         <p><FaArrowRight></FaArrowRight></p>
+         </div>
+
+        
+
+
+         <div className="flex items-center gap-3 mb-36">
+      <p className="rounded-[50%] text-[15px] border-[1px] border-gray-600 p-[7px] bg-inherit hover:bg-white hover:text-black hover:border-[1px] hover:border-white duration-500"><FaFacebookF /></p>
+      <p className="rounded-[50%] text-[15px] border-[1px] border-gray-600 p-[7px] bg-inherit hover:bg-white hover:text-black hover:border-[1px] hover:border-white duration-500"><FaTwitter /></p>
+      <p className="rounded-[50%] text-[15px] border-[1px] border-gray-600 p-[7px] bg-inherit hover:bg-white hover:text-black hover:border-[1px] hover:border-white duration-500"><FaInstagram /></p>
+      <p className="rounded-[50%] text-[15px] border-[1px] border-gray-600 p-[7px] bg-inherit hover:bg-white hover:text-black hover:border-[1px] hover:border-white duration-500"><FaDribbble /></p>
     </div>
-  </nav>
-</footer>
+
+
+
+        </div>
+
+        {/* second part */}
+
+        <div className='flex justify-between  w-1/2 '>
+
+        {/* 1 */}
+          <div className='flex-col items-start'>
+
+            <div className='mb-8'>
+              <h2 className='text-3xl font-normal mb-4'>Address</h2>
+              <p className='text-gray-400 font-sans text-lg font-semibold mb-2 hover:text-white duration-500'>16 Road No.11 Uttara, <br />Dhaka 1230</p>
+            </div>
+
+            <div>
+              <h2 className='text-3xl font-normal mb-4'>Contact</h2>
+              <p className='text-gray-400 font-sans text-lg font-semibold mb-2 hover:text-white duration-500'>+880 1718-854866</p>
+              <p className='text-gray-400 font-sans text-lg font-semibold mb-2 hover:text-white duration-500'>octalink@gmail.com</p>
+            </div>
+
+          </div>
+
+          {/* 2 */}
+          
+          <div className='flex flex-col items-start'>
+            <h2 className='text-3xl font-normal mb-4'>Explore</h2>
+            <Link to='/'><p className='text-gray-400 font-sans text-lg font-semibold mb-2 hover:text-white duration-500'>Home</p></Link>
+            <Link to='/about'><p className='text-gray-400 font-sans text-lg font-semibold mb-2 hover:text-white duration-500'>About</p></Link>
+            <Link to='/products'><p className='text-gray-400 font-sans text-lg font-semibold mb-2 hover:text-white duration-500'>Products</p></Link>
+            <Link to='/partners'><p className='text-gray-400 font-sans text-lg font-semibold mb-2 hover:text-white duration-500'>Partners</p></Link>
+            <Link to='/contact'><p className='text-gray-400 font-sans text-lg font-semibold mb-2 hover:text-white duration-500'>Contacts</p></Link>
+            
+          </div>
+
+          {/* 3 */}
+          
+          <div>
+            <h2 className='text-3xl font-normal mb-4'>Resources</h2>
+            <p className='text-gray-400 font-sans text-lg font-semibold mb-2 hover:text-white duration-500'>Documentation</p>
+            <p className='text-gray-400 font-sans text-lg font-semibold mb-2 hover:text-white duration-500'>Privacy Policy</p>
+            <p className='text-gray-400 font-sans text-lg font-semibold mb-2 hover:text-white duration-500'>Press Files</p>
+            <p className='text-gray-400 font-sans text-lg font-semibold mb-2 hover:text-white duration-500'>FAQs</p>
+            <p className='text-gray-400 font-sans text-lg font-semibold mb-2 hover:text-white duration-500'>Contact</p>
+          </div>
+
+        </div>
+
+
+      </div>
+
+      </div>
+
+    <div className='bg-gray-600 h-[1px]'></div>
+
+    <div className='flex justify-between py-4 mx-auto container'>
+
+      <h2 className='font-sans font-normal'>Copyright © 2024 - All right reserved by Octalink Global Sourcing</h2>
+      
+      <div className='flex gap-4'>
+        <p className='font-sans font-normal'>Privacy Policy</p>
+        <p className='font-sans font-normal'>Terms & Services</p>
+      </div>
+    </div>
+
+  </footer>
   );
 };
 
